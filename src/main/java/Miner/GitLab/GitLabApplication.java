13d88c255719dd26bec141e76cb7adbe8ee408ce
Pablo Rodriguez <pabrodsan11@alum.us.es>
2023-05-04 20:42:14 +0200

    Initial commit

---
+++ src/main/java/Miner/GitLab/GitLabApplication.java
@@ -0,0 +1,13 @@
+package Miner.GitLab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitLabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Miner/GitLab/GitLabApplicationTests.java
@@ -0,0 +1,13 @@
+package Miner.GitLab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
